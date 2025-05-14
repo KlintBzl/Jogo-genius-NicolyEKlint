7b3859a78ec0a510165f425e07013b7ce0d2d3a6
KlintBzl <klintscouts@gmail.com>
2025-05-14 17:53:50 -0300

    facil

--- facil/facil.js
+++ facil/facil.js
@@ -5,10 +5,41 @@ let nivel = 0;
 let aceitandoInput = false;
 
 const startBtn = document.getElementById('btn-start');
-const atualizacaoText = document.getElementById('text-atualizar');
+const atualizarText = document.getElementById('text-atualizar');
 
 startBtn.addEventListener('click', startgame);
 
 cores.forEach(cor => {
-    document.getElementById(cor).addEventListener('click')
-})
+    document.getElementById(cor).addEventListener('click', () => handleClick(cor));
+});
+
+function comecarJogo(){
+    sequencia = [];
+    nivel = 0;
+    atualizarText.textContent = "Boa sorte!";
+    proximoN();
+}
+
+function proximoN() {
+    sequenciaDoJogador = [];
+    nivel++;
+    atualizarText.textContent = `Nível ${nivel}`;
+    const proxCor = cores[Math.floor(Math.random() * cores.length)];
+    sequencia.push(proxCor);
+    sequenciar();
+}
+
+function sequenciar(){
+    aceitandoInput = false;
+    let i = 0;
+    const intervalo = setInterval(() =>  {
+
+    const cor = sequence[i];
+    corFlash(cor);
+    i++;
+    if (i >= sequence.length){
+        clearInterval(interval)
+    }
+            
+    }) 
+}
