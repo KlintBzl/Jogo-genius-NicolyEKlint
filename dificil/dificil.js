7822386250ca920f1bc774ff40602501a2d9ebb0
KlintBzl <klintscouts@gmail.com>
2025-05-21 16:54:03 -0300

    blabla

--- dificil/dificil.js
+++ dificil/dificil.js
@@ -102,5 +102,15 @@ function handleClick(color) {
   }
 }
 
+const btnAlternarE = document.getElementById("alternarE");
+const btnAlternarC = document.getElementById("alternarC");
+const troca = document.querySelector("body");
+btnAlternarE.addEventListener("click", (e) => {
+      troca.classList.add("escurecerB")
 
-    
+});
+
+btnAlternarC.addEventListener("click", (e) => {
+      troca.classList.remove("escurecerB")
+
+})
--- facil/facil.js
+++ facil/facil.js
@@ -115,10 +115,11 @@ const btnAlternarE = document.getElementById("alternarE");
 const btnAlternarC = document.getElementById("alternarC");
 const troca = document.querySelector("body");
 btnAlternarE.addEventListener("click", (e) => {
-      troca.classList.add("clarear")
+      troca.classList.add("escurecerB")
+
 });
 
 btnAlternarC.addEventListener("click", (e) => {
-      troca.classList.remove("clarear")
-})
+      troca.classList.remove("escurecerB")
 
+})
