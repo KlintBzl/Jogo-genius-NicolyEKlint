b218e6fb36c5e768b193208ef4e86a40106398d1
KlintBzl <klintscouts@gmail.com>
2025-05-23 14:45:38 -0300

    mudo

--- facil/facil.js
+++ facil/facil.js
@@ -133,9 +133,7 @@ btnAlternarE.addEventListener("click", (e) => {
       statusText.classList.remove("escurecerText");
 })
 
-const jogar = new Audio('/sons/gaming-music-8-bit-console-play-background-intro-theme-342069.mp3')
-jogar.loop = true;
-jogar.autoplay = true;
+const jogar = document.getElementById("jogue");
 let numerarSom = 0;
 
 const btnAmbiente = document.getElementById("Mudo");
